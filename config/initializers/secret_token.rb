4c5c35b98602fba07b7de16e337a9b6c299fb9da
harryheintz <harryheintz@me.com>
2011-10-31 14:23:16 -0400

    Bushwood test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bushwood::Application.config.secret_token = '708b576d61c457d950cd793f775d11b70611da67f78c7c1c36d44505f23de294bdd02f7641f354f7c8a16d31f9e2a4a219e46549611e670c06eae4d9e32c5ee1'
